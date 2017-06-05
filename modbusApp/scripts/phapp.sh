e163827edb95be2691eb7883ada3489643b9bdf6
Srinivas Chivukula <srichivu@cisco.com>
2017-06-05 16:54:23 +0530

    a sample app that configures modbus device model

---
+++ modbusApp/scripts/phapp.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+if [ -f /data/.env ]
+then
+	source /data/.env
+fi
+
+start_app() {
+	cd /usr/share/phapp
+	python ph_app.py 
+}
+
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$pid" 2>/dev/null
+}                              
+                               
+trap _term SIGTERM
+
+case "$1" in
+start)
+        echo -n "Starting app phapp"
+        start_app
+        echo "done."
+        ;;
+
+stop)
+        echo -n "Stopping app phapp"
+        pidof python | xargs kill -15
+        : exit 0
+        echo "done."
+        ;;
+
+force-reload|restart)
+        echo "Reconfiguring phapp"
+        echo "done."
+        ;;
+
+*)
+        echo "Usage: /etc/init.d/phapp {start|stop}"
+        exit 1
+        ;;
+esac
+
+exit 0
