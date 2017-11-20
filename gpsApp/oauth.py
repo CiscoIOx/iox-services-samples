178f4e44c82c6fb6c0834dc4dbdd53c0e60bdc5e
mrathees <mrathees@cisco.com>
2017-11-20 23:18:09 +0530

    gps oauthurl change

--- gpsApp/oauth.py
+++ gpsApp/oauth.py
@@ -9,7 +9,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 client_id = os.environ["OAUTH_CLIENT_ID"]
 client_secret = os.environ["OAUTH_CLIENT_SECRET"]
-token_url = os.environ["OAUTH_TOKEN_URL"]
+server_ip = os.environ["OAUTH_TOKEN_SERVER_IPV4"]
+server_port = os.environ["OAUTH_TOKEN_SERVER_PORT"]
+api_path = os.environ["OAUTH_TOKEN_API_PATH"]
+token_url = "https://"+server_ip+":"+server_port+api_path
 
 print "client_id: "+client_id
 print "client_secret: "+client_secret
